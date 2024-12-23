b39349fc0786c9e4d04e097dd8279c45f4f1b04e
Lankiman <marvellousgboun@gmail.com>
2024-12-23 01:27:25 +0100

    implmenting services

---
+++ Models.cs
@@ -0,0 +1,44 @@
+namespace better_submitter_api;
+
+public class GeneralDataModel
+{
+    public string StudentId { get; set; } 
+    public StudentDepartment Department { get; set; }
+}
+
+public class SubmissionDataModel:GeneralDataModel
+{
+    public string SurnName { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string? MiddleName { get; set; }
+    
+    public Dictionary<int,AssignmentSubmisssion> SubmitedAssigmentFiles { get; set; }
+    
+    public Dictionary<int,AssignmentSubmisssion> SubmitedAssigmentVideos { get; set; }
+}
+
+public class AssignmentSubmisssion
+{
+    public int SubmissionCount { get; set; }
+    public bool CanBeResubmitted { get; set; }
+}
+public enum AssigmentType
+{
+    C,
+    Python
+}
+
+public enum StudentDepartment
+{
+    Agricultural,
+    Civil,
+    Chemical,
+    Electrical,
+    Mechanical,
+    Marine,
+    Petroleum,
+    
+    
+}
--- Services.cs
+++ Services.cs
@@ -2,14 +2,51 @@ namespace better_submitter_api;
 
 public static class Services
 {
-    private static readonly string _contentRootPath;
     
     private static IWebHostEnvironment _hostingEnvironment;
     
-    private static 
+    private static readonly string _contentRootPath;
+    
+    private static readonly string _generalDataPath = Path.Combine(_contentRootPath, "Data", "general_submission_data.csv");
+    
+    private static readonly string _cDataPath = Path.Combine(_contentRootPath, "Data", "c_submission_data.csv");
+    
+    private static readonly string _pythongDataPath = Path.Combine(_contentRootPath, "Data", "python_submission_data.csv");
+    
+    private static Dictionary<string, GeneralDataModel> GeneralSubmissionData { get; set; } = new();
+    
+    private static Dictionary<string, GeneralDataModel> CSubmissionData { get; set; } = new();
+    
+    private static Dictionary<string, GeneralDataModel> PythonSubmissionData { get; set; } = new();
+
     
     static Services()
     {
         _contentRootPath = _hostingEnvironment.ContentRootPath;
+    }
+    
+    private static void PopulateDataDicionaries()
+    {
+        if (File.Exists(_generalDataPath))
+        {
+            var data= File.ReadAllLines(_generalDataPath).ToList();
+            GeneralSubmissionData = data.ConvertToGeneralDataModel();
+        }
+       
+    }
+    
+    private static Dictionary<string, GeneralDataModel> ConvertToGeneralDataModel(this List<string> data)
+    {
+      
+        return data
+            .Select(line => line.Split(","))
+            .ToDictionary(
+                lineData => lineData[0],
+                lineData => new GeneralDataModel
+                {
+                    StudentId = lineData[0],
+                    Department = Enum.Parse<StudentDepartment>(lineData[1])
+                }
+            );
     }
 }
